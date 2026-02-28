04e61b9622388551d9e3ad5d6af2ddf326a9d89e
Tobias Hargesheimer <tobias@ison.ws>
2026-02-28 01:27:58 +0100

    some changes

--- alpine.multiarch.Dockerfile
+++ alpine.multiarch.Dockerfile
@@ -1,3 +1,4 @@
+# build: docker build --no-cache --progress=plain -t docker.io/tobi312/prosody:latest -f alpine.multiarch.Dockerfile .
 FROM alpine:latest
 
 SHELL ["/bin/sh", "-euxo", "pipefail", "-c"]
@@ -5,42 +6,35 @@ SHELL ["/bin/sh", "-euxo", "pipefail", "-c"]
 ARG VCS_REF
 ARG BUILD_DATE
 
+ENV LUA_VERSION=5.4
+ENV __FLUSH_LOG=yes
+
 LABEL org.opencontainers.image.authors="Tobias Hargesheimer <docker@ison.ws>" \
       org.opencontainers.image.title="prosody" \
       org.opencontainers.image.description="Prosody (XMPP/Jabber)" \
       org.opencontainers.image.created="${BUILD_DATE}" \
       org.opencontainers.image.revision="${VCS_REF}" \
       org.opencontainers.image.licenses="Apache-2.0" \
+      org.opencontainers.image.documentation="https://prosody.im/doc" \
       org.opencontainers.image.url="https://hub.docker.com/r/tobi312/prosody" \
       org.opencontainers.image.source="https://github.com/Tob1as/docker-prosody"
 
 RUN set -eux; \
     apk --no-cache add \
-    #git wget curl nano zip unzip \
-    tzdata \
-    libidn \
-    icu-libs \
-    libssl3 \
-    libevent-dev \
-    libpq-dev \
-    sqlite-dev \
-    lua5.2-bitop \
-    lua5.2-dbi-mysql \
-    lua5.2-dbi-postgresql \
-    lua5.2-dbi-sqlite3 \
-    lua5.2-cqueues \
-    lua5.2-expat \
-    lua5.2-filesystem \
-    lua5.2-sec \
-    lua5.2-socket \
-    lua5.2-lzlib \
-    lua5.2-ldap \
-    lua5.2 \
-    luarocks5.2 \
-    openssl \
-    ca-certificates \
-    prosody \
-    mercurial \
+        tzdata \
+        ca-certificates \
+        tini \
+        runuser \
+        #lua${LUA_VERSION} lua${LUA_VERSION}-socket lua${LUA_VERSION}-sec lua${LUA_VERSION}-expat lua${LUA_VERSION}-filesystem lua${LUA_VERSION}-unbound \
+        prosody \
+        lua${LUA_VERSION}-dbi-mysql \
+        lua${LUA_VERSION}-dbi-postgresql \
+        lua${LUA_VERSION}-dbi-sqlite3 \
+        lua${LUA_VERSION}-ldap \
+        lua${LUA_VERSION}-bitop \
+        #lua${LUA_VERSION}-dev \
+        luarocks${LUA_VERSION} \
+        mercurial \
     ; \
     cp /etc/prosody/prosody.cfg.lua /etc/prosody/prosody.cfg.lua.backup ; \
     mkdir -p /etc/prosody/conf.d ; \
@@ -57,12 +51,12 @@ RUN set -eux; \
 
 #COPY entrypoint.d/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
 
-## https://prosody.im/doc/ports (proxy, c2s, s2s, http, https, components)
-EXPOSE 5000 5222 5269 5280 5281 5347
+## https://prosody.im/doc/ports (proxy, c2s, s2s, http, https, components, telnet)
+EXPOSE 5000/tcp 5222/tcp 5269/tcp 5280/tcp 5281/tcp 5347/tcp 5582/tcp
 
-VOLUME ["/etc/prosody/", "/etc/prosody/conf.d/", "/usr/lib/prosody/modules-custom"]
-
+# uid=100 gui=101
 #USER prosody
 
-ENTRYPOINT ["entrypoint.sh"]
-CMD ["prosody"]
+#ENTRYPOINT ["entrypoint.sh"]
+ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/entrypoint.sh"]
+CMD ["prosody", "-F", "--config /etc/prosody/prosody.cfg.lua"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,34 +9,19 @@ set -eu
 : "${ENABLE_CONFD:="0"}"                      # set 1 to enable
 : "${ENABLE_MODULE_PATHS:="0"}"               # set 1 to enable
 
-lsb_dist="$(. /etc/os-release && echo "$ID")" # get os (example: debian or alpine) - do not change!
+# register user
+: "${LOCAL:=""}"     # user
+: "${PASSWORD:=""}"  # password
+: "${DOMAIN:=""}"    # domain
 
-## set TimeZone
-if [ -n "$TZ" ]; then
-	echo ">> set timezone to ${TZ} ..."
-	#if [ "$lsb_dist" = "alpine" ]; then apk add --no-cache --virtual .fetch-tmp tzdata; fi
-	#ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
-	cp /usr/share/zoneinfo/${TZ} /etc/localtime
-	echo ${TZ} >  /etc/timezone
-	#if [ "$lsb_dist" = "alpine" ]; then apk del --no-network .fetch-tmp; fi
-	date
-fi
+#lsb_dist="$(. /etc/os-release && echo "$ID")" # get os (example: debian or alpine) - do not change!
 
-## select modules, separate with spaces (symlink will create from available to enable)
-if [ -n "$SELECT_COMMUNITY_MODULES" ]; then
-	#echo ">> set community modules: ${SELECT_COMMUNITY_MODULES}"
-	echo ">> set community modules:"
-	#reset_IFS=$IFS
-	#IFS=' '
-	for c_module in $SELECT_COMMUNITY_MODULES; do
-		echo "-> $c_module"
-		ln -sf /usr/lib/prosody/modules-community-available/${c_module} /usr/lib/prosody/modules-community-enable/${c_module}
-	done
-	#IFS=$reset_IFS
+if [ "$(id -u)" -eq 0 ]; then
+	[ -d "/entrypoint.d/" ] || mkdir /entrypoint.d/
 fi
 
 # more entrypoint-files
-for f in /entrypoint.d/*; do
+find "/entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
 	#echo ">> entrypoint-file: $f"
 	case "$f" in
 		*.sh)
@@ -59,38 +44,58 @@ for f in /entrypoint.d/*; do
 	esac
 done
 
-## enable conf.d (only need if use default config and conf.d folder)
-if [ "$ENABLE_CONFD" -eq "1" ] ; then
-	grep -qxF 'Include "conf.d/*.cfg.lua"' /etc/prosody/prosody.cfg.lua || echo 'Include "conf.d/*.cfg.lua"' >> /etc/prosody/prosody.cfg.lua
-	echo ">> enable conf.d"
-fi
+if [ "$(id -u)" -eq 0 ]; then
+	## select modules, separate with spaces (symlink will create from available to enable)
+	if [ -n "$SELECT_COMMUNITY_MODULES" ]; then
+		#echo ">> set community modules: ${SELECT_COMMUNITY_MODULES}"
+		echo ">> set community modules:"
+		#reset_IFS=$IFS
+		#IFS=' '
+		for c_module in $SELECT_COMMUNITY_MODULES; do
+			echo "-> $c_module"
+			ln -sf /usr/lib/prosody/modules-community-available/${c_module} /usr/lib/prosody/modules-community-enable/${c_module}
+		done
+		#IFS=$reset_IFS
+	fi
 
-## enable module path (see dockerfile)
-if [ "$ENABLE_MODULE_PATHS" -eq "1" ] ; then
-	sed -i "s/--plugin_paths = {}/plugin_paths = { \"/usr/lib/prosody/modules\" , \"/usr/lib/prosody/modules-community-enable\", \"/usr/lib/prosody/modules-custom\" }/" /etc/prosody/prosody.cfg.lua
-	echo ">> enable and set module path (see dockerfile)"
+	## enable conf.d (only need if use default config and conf.d folder)
+	if [ "$ENABLE_CONFD" -eq "1" ] ; then
+		grep -qxF 'Include "/etc/prosody/conf.d/*.cfg.lua"' /etc/prosody/prosody.cfg.lua || echo 'Include "/etc/prosody/conf.d/*.cfg.lua"' >> /etc/prosody/prosody.cfg.lua
+		echo ">> enable /etc/prosody/conf.d/*.cfg.lua"
+	fi
+
+	## enable module path (see dockerfile)
+	if [ "$ENABLE_MODULE_PATHS" -eq "1" ] ; then
+		sed -i "s|--plugin_paths = {}|plugin_paths = { \"/usr/lib/prosody/modules\" , \"/usr/lib/prosody/modules-community-enable\", \"/usr/lib/prosody/modules-custom\" }|" /etc/prosody/prosody.cfg.lua
+		echo ">> enable and set module path (see dockerfile)"
+	fi
 fi
 
 ## use shell (docker run --name prosody -it $imagename sh)
 if [[ "$1" == "sh" ]]; then
 	exec sh
 fi
+if [[ "$1" == "ash" ]]; then
+	exec ash
+fi
 if [[ "$1" == "bash" ]]; then
 	exec bash
 fi
 
-## ...
+# 
 if [[ "$1" != "prosody" ]]; then
-	exec prosodyctl $*
-	exit 0;
+    exec prosodyctl "$@"
+    exit 0;
 fi
 
-## ...
-if [ "$LOCAL" -a  "$PASSWORD" -a "$DOMAIN" ] ; then
-	prosodyctl register "$LOCAL" "$DOMAIN" "$PASSWORD"
+# register user
+if [[ "$LOCAL" && "$PASSWORD" && "$DOMAIN" ]]; then
+    prosodyctl register "$LOCAL" "$DOMAIN" "$PASSWORD"
 fi
 
 # exec CMD
 echo ">> exec docker CMD"
-echo "$@"
-exec "$@"
+#echo "$@"
+#exec "$@"
+echo "runuser -u prosody -- $@"
+exec runuser -u prosody -- "$@"
